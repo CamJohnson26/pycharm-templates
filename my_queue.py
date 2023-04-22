b59af7921d0d8789b7db8712f4bc2cc2fa09c245
Cameron Johnson <cameronrossjohnson@gmail.com>
2023-04-22 13:28:11 -0500

    😵 Finally got dead letter queues working

---
+++ my_queue.py
@@ -0,0 +1,24 @@
+"""This module contains the queue name and callback function
+"""
+
+import aio_pika
+MY_QUEUE_NAME = 'my_queue'
+
+
+async def my_queue_callback(message: aio_pika.IncomingMessage):
+    """Callback function for the queue
+
+    This function will be called when a message is received from the queue."""
+    try:
+        print(f" [x] Received {message.body}")
+        await message.ack()
+    except Exception as e:
+        print("Error:", e)
+        await message.reject(requeue=False)
+
+
+def get_my_queue() -> tuple[str, callable]:
+    """Get the queue name and callback function
+
+    This function will return the queue name and callback function."""
+    return MY_QUEUE_NAME, my_queue_callback
